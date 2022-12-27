dec20e38541fc6958cb966a79d008b0b927bb7d1
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2022-12-27 17:58:44 +0300

    Add logger

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"github.com/julienschmidt/httprouter"
-	"log"
 	"net"
 	"net/http"
 	"rest-api-tutorial/internal/user"
+	"rest-api-tutorial/pkg/logging"
 	"time"
 )
 
 func main() {
-	log.Println("create router")
+	logger := logging.GetLogger()
+	logger.Info("Create router")
 	router := httprouter.New()
 
-	log.Println("register user new handler")
-	handler := user.NewHandler()
+	logger.Info("register user new handler")
+	handler := user.NewHandler(logger)
 	handler.Register(router)
 
 	start(router)
 }
 
 func start(router *httprouter.Router) {
-	log.Println("start application")
+	logger := logging.GetLogger()
+	logger.Info("start application")
 
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
@@ -34,6 +36,6 @@ func start(router *httprouter.Router) {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	log.Println("server is listening port 1234")
-	log.Fatal(server.Serve(listener))
+	logger.Info("server is listening port 1234")
+	logger.Fatal(server.Serve(listener))
 }
