e981f72f24d6449e16feaa92400c42aed2002d3a
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2022-12-30 23:41:16 +0300

    Add connection to db

---
+++ internal/user/service.go
@@ -0,0 +1,16 @@
+package user
+
+import (
+	"context"
+	"rest-api-tutorial/pkg/logging"
+)
+
+type Service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context, dto CreateUserDTO) (u User, err error) {
+	// ToDo next one
+	return
+}
